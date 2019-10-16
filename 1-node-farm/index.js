1d87f62c08142e7e0339bddd73240a46f7021cf7
DanierJ <danierjavid@gmail.com>
2019-10-16 15:58:54 -0500

    using modules 1 (core modules) - reading sync a file

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,8 @@
-const hello = 'Hello World';
-console.log(hello);
-
 // Reading file system(fs module)
 const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', "utf-8");
+
+console.log(textIn);
+
+
