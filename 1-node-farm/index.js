556f2c17b5cdc4a9a6d19bddc779fe9b875aec12
DanierJ <danierjavid@gmail.com>
2019-10-16 17:01:22 -0500

    using modules 1 (core modules) - creating a simple web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,6 @@
 
-// Reading file system(fs module) - Blocking, synchronous way.
-const fs = require('fs');
+///////////////// Reading file system(fs module) - Blocking, synchronous way. ////////////
+/*const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', "utf-8");
 //console.log(textIn);
@@ -11,18 +11,40 @@ fs.writeFileSync('./txt/output.txt', textOut);
 //console.log('File written!');
 
 
-// Non-blocking, asynchronous way
+//////////////////// Non-blocking, asynchronous way /////////////////
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     if (err) return console.log('ERROR!');
     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-      console.log(`data2: ${data2}`);
+      //console.log(`data2: ${data2}`);
       fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-          console.log(`data3: ${data3}`);
+       //   console.log(`data3: ${data3}`);
           fs.writeFile('./txt/final.txt', `${data2}\n ${data3}` , err => {
-            console.log('Your file has been written.')
+       //     console.log('Your file has been written.')
           })
       });
     });
-});
+});*/
 // console.log('Your file is being read');
+
+/////////////////// Creating a simple web server //////////////////////
+const http = require('http');
+
+/**
+ * In order to built our server
+ *
+ * 1. We create the server.
+ * 2. We start the server.
+ */
+
+// 1. We create the server, the callback it's hit with every new request.
+const server = http.createServer((request, response) => {
+ // console.log(request);
+  response.end('Hello from the server!'); // here we're sending a response.
+});
+
+// 2. We need to start the server.
+server.listen(8585, '127.0.0.1', () => {
+  console.log('Listening to request on port: 8585');
+});
+
 
