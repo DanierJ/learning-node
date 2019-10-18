09f60621df8044223bba7c3135018eb09243f6fc
DanierJ <danierjavid@gmail.com>
2019-10-18 17:08:34 -0500

    using modules 1 (core modules) - HTML templating filling the templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -52,26 +52,62 @@ const url = require('url');
 const fs = require('fs');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+
 const productData = JSON.parse(data);
 
+
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCT_NAME%}/g, product.productName);
+
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION}/g, product.description);
+    output = output.replace(/{%PRODUCT_ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%ORGANIC%}/g, 'not-organic');
+
+    return output;
+};
 
 const server = http.createServer((request, response) => {
   console.log(request.url);
 
   const pathName = request.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    response.end('This is the overview.');
 
+
+    response.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+
+    const cardsHTML = productData.map(product => replaceTemplate(tempCard, product)).join(''); // without join also works
+    const finalOverview = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHTML);
+
+    response.end(finalOverview);
+
+
+  // Product page
   } else if(pathName === '/product') {
     response.end('This is the product.');
 
+  // API
   } else if(pathName === '/api') {
       response.writeHead(200, {
         'Content-type': 'application/json'
       });
       response.end(data);
 
+  // NOT FOUND
   } else {
     response.writeHead(404, {
       // headers
