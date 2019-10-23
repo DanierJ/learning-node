d5d5e0489c9659f67a3248f9c76bdc84d6db2a04
DanierJ <danierjavid@gmail.com>
2019-10-23 11:56:29 -0500

    from callback hell to promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -6,14 +6,17 @@ const superagent = require('superagent');
 
 fs.readFile('./dog.txt', (err, data) => {
   const hound = data.toString().trim();
-  superagent.get(`https://dog.ceo/api/breed/${hound}/images/random`).end((err, res) => {
-      if (err) return console.log(err.message);
+  superagent
+    .get(`https://dog.ceo/api/breed/${hound}/images/random`)
+    .then(res => {
       console.log(res.body.message);
 
       fs.writeFile('dog-img.txt', res.body.message, err => {
         if (err) return console.log(err.message);
         console.log('Random dog image saved to file!');
       });
+  }).catch(err => {
+    return console.log(err.message);
   });
 });
 
