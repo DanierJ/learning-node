68973365159a6f4b12924d5e65799d92452ea446
DanierJ <danierjavid@gmail.com>
2019-10-23 16:51:30 -0500

    building promises

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -2,9 +2,25 @@ const fs = require('fs');
 const superagent = require('superagent');
 
 
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+          if (err) reject('I could not find that file.');
+          resolve(data);
+        });
+  });
+};
 
+const writeFilePro = (file, data) => {
+  return new Promise(((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('Could not write the file');
+      resolve('success');
+    });
+  }));
+};
 
-fs.readFile('./dog.txt', (err, data) => {
+/*fs.readFile('./dog.txt', (err, data) => {
   const hound = data.toString().trim();
   superagent
     .get(`https://dog.ceo/api/breed/${hound}/images/random`)
@@ -18,6 +34,15 @@ fs.readFile('./dog.txt', (err, data) => {
   }).catch(err => {
     return console.log(err.message);
   });
-});
+});*/
 
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    const hound = data.toString().trim();
+
+    return superagent.get(`https://dog.ceo/api/breed/${hound}/images/random`)})
+      .then(res => writeFilePro('dog-img.txt', res.body.message))
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
 
