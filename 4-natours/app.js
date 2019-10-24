d8ce568cd31c5f3b8843c4e6a6fa38bdb40be78b
DanierJ <danierjavid@gmail.com>
2019-10-24 16:20:07 -0500

    natours project - starting our api handling GET requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,17 +1,26 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
-});
-
-app.post('/', (req, res) => {
-  res.status(200).json({message: 'Response from the post action', app: 'Natours'});
-});
-
 const port = 3100;
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+
 
 
