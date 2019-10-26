5c5baa138ed5a4ab2eda2184ce871aceeffb83dd
DanierJ <danierjavid@gmail.com>
2019-10-26 16:58:21 -0500

    natours project - serving static files

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -9,6 +9,7 @@ const app = express();
 /// 1 MIDDLEWARE
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // applied to each and every single request.
 app.use((req, res, next) => {
