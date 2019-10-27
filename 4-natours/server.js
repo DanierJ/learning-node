e700fcb4c7cc1d2650bffed27f44e3be456e5692
DanierJ <danierjavid@gmail.com>
2019-10-27 11:28:53 -0500

    natours project - environment variables

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,4 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
 const app = require('./app');
-const port = 3100;
+const port = process.env.PORT || 3100;
+
+
+// console.log(app.get('env')); // environment
+// console.log(process.env);
+// NODE_ENV=development nodemon server.js
 
 app.listen(port, () => console.log(`App running on port ${port}...`));
