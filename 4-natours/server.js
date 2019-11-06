561a422e5a830c1148f99fd38fba46dcfcf9b222
DanierJ <danierjavid@gmail.com>
2019-11-06 15:36:20 -0500

    natours project - creating a simple tour model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -6,6 +6,7 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
+
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -21,6 +22,26 @@ mongoose
     // eslint-disable-next-line no-console
     console.log(err);
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name.'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price.']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
 const app = require('./app');
 
 const port = process.env.PORT || 3100;
