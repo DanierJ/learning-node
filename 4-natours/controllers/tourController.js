2afc00d8b6f35dc8ae761ce09c6f00a03fe5424e
DanierJ <danierjavid@gmail.com>
2019-11-07 15:42:32 -0500

    natours project - sorting

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -8,12 +8,20 @@ exports.getAllTours = async (req, res) => {
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced query
+    // 1B) Advanced query
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
     // BUILD QUERY
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
