a4a92feabc51d6bbe2b6f962706eb9c942070600
DanierJ <danierjavid@gmail.com>
2019-11-07 15:30:47 -0500

    natours project - advanced filtering

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,13 +2,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
+    // 1) Filtering
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
     const queryObj = { ...req.query }; // copying an object
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach(el => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 2) Advanced query
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
 
+    // BUILD QUERY
+    const query = Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
     const tours = await query;
 
     res.status(200).json({
