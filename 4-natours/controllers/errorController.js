32811a88a7bc9d7702e4d602e75483dc814425fb
DanierJ <danierjavid@gmail.com>
2019-11-15 17:29:19 -0500

    natours project - handling invalid database ids

--- 4-natours/controllers/errorController.js
+++ 4-natours/controllers/errorController.js
@@ -1,11 +1,26 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack
+      message: err.message
     });
     // Programming or other unknown error: don't leak error details
   } else {
@@ -20,20 +35,21 @@ const sendErrorDev = (err, res) => {
   }
 };
 
-const sendErrorProd = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-};
-
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    console.log('HELLO DEVELOPMENT');
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    console.log('HELLO PRODUCTION');
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    let error = { ...err };
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
   }
 };
