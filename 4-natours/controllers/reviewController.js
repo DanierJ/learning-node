198becefd13dc35b7cac2cb64351bf0d1994e788
Danier Javid <danierjavid@gmail.com>
2019-12-05 17:14:13 -0500

    natours project - building handler factory functions update and create

--- 4-natours/controllers/reviewController.js
+++ 4-natours/controllers/reviewController.js
@@ -2,20 +2,6 @@ const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.createReview = catchAsync(async (req, res, next) => {
-  // Allow nested routes
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-  const newReview = await Review.create(req.body);
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      review: newReview
-    }
-  });
-});
-
 exports.getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
 
@@ -31,5 +17,16 @@ exports.getAllReviews = catchAsync(async (req, res, next) => {
     }
   });
 });
+
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+
+exports.createReview = factory.createOne(Review);
+
+exports.updateReview = factory.updateOne(Review);
 
 exports.deleteReview = factory.deleteOne(Review);
