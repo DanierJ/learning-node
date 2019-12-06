5a720e9e8d748b6ff258e3d5d67a9b5f3c87806b
Danier Javid <danierjavid@gmail.com>
2019-12-06 16:09:02 -0500

    natours project - importing review and user data

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
+const User = require('./../../models/userModel');
+const Review = require('./../../models/reviewModel');
 
 dotenv.config({ path: `${__dirname}/../../config.env` });
 const DB = process.env.DATABASE.replace(
@@ -27,11 +29,17 @@ mongoose
 
 // READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // Import data into db
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     // eslint-disable-next-line no-console
     console.log('Data successfully loaded.');
   } catch (err) {
@@ -45,6 +53,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     // eslint-disable-next-line no-console
     console.log('Data successfully deleted.');
   } catch (err) {
