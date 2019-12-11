13a0ec3af7088275d835e99cf05a6bd99a84a1d0
Danier Javid <danierjavid@gmail.com>
2019-12-11 13:23:03 -0500

    natours project - building the tour page

--- 4-natours/controllers/viewsController.js
+++ 4-natours/controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const Review = require('./../models/reviewModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
@@ -14,8 +15,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) get the data for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  // 2) Build template
+
+  // 3) Render template using data from step 1
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour
+  });
+});
