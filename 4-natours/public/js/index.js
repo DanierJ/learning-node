b6ceacb7f9af5bc1aac0f3f970a0016ac2c55816
Danier Javid <danierjavid@gmail.com>
2019-12-14 11:01:14 -0500

    natours project - updating user password with our api

--- 4-natours/public/js/index.js
+++ 4-natours/public/js/index.js
@@ -2,11 +2,14 @@
 import '@babel/polyfill';
 import { displayMap } from "./mapbox";
 import { login, logout } from './login';
+import { updateSettings } from "./updateSettings";
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 // VALUES
 
@@ -29,3 +32,29 @@ if (loginForm) {
 }
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit',  (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+
+    updateSettings({name, email}, 'data');
+  })
+}
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit',  async e => {
+    e.preventDefault();
+    const passwordCurrent = document.getElementById('password-current');
+    const password = document.getElementById('password');
+    const passwordConfirm = document.getElementById('password-confirm');
+    const btnSavePassword = document.querySelector('.save-password').textContent = 'Updating...';
+
+    await updateSettings({passwordCurrent, password, passwordConfirm}, 'password');
+
+    btnSavePassword.textContent = 'Save password';
+    passwordCurrent.value = '';
+    password.value = '';
+    passwordConfirm.value = '';
+  })
+}
